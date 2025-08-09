ae44854ac15efe30384925265dffc7bd8870a628
BOT_TOKEN <meho1464@gmail.com>
2025-08-09 06:45:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-#telegram-bot 
 import os
 import telebot
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("توکن BOT_TOKEN پیدا نشد. لطفاً تو Railway اضافه کن.")
+
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=["start"])
 def start_handler(message):
-    bot.send_message(message.chat.id, "سلام! ربات فعال است.")
+    bot.send_message(message.chat.id, "سلام! ربات فعال است ✅")
 
-print("Bot is running...")
+print("ربات روشن شد...")
 bot.infinity_polling()
